51c34fb721aed228ee655f884f91d27b53c46999
KimChaiWon <kcw030220@naver.com>
2023-11-19 23:02:28 +0900

    네비게이션 하단바까지

---
+++ app/src/main/java/com/example/scheduleapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.scheduleapp
+
+import android.icu.text.DateFormat
+import android.icu.text.SimpleDateFormat
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.TextView
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.scheduleapp.databinding.ActivityMainBinding
+import java.sql.Date
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val navController = binding.frgNav.getFragment<NavHostFragment>().navController
+        setupActionBarWithNavController(navController)
+        binding.bottomNav.setupWithNavController(navController)
+        setContentView(binding.root)
+        //객체생성
+        
+        }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController=binding.frgNav.getFragment<NavHostFragment>().navController
+        return navController.navigateUp()||super.onSupportNavigateUp()
+    }
+}
