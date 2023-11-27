9f40810fb548c885e29d5655603e4bf708640098
KimChaiWon <kcw030220@naver.com>
2023-11-27 14:46:09 +0900

    그냥 db없앰

--- app/src/main/java/com/example/scheduleapp/calendarFragment.kt
+++ app/src/main/java/com/example/scheduleapp/calendarFragment.kt
@@ -1,19 +1,12 @@
 package com.example.scheduleapp
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.scheduleapp.databinding.FragmentCalendarBinding
-import com.google.firebase.Firebase
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.database
-import com.google.firebase.database.getValue
 
 class calendarFragment : Fragment() {
     private lateinit var todoAdapter: TodoAdapter
@@ -33,24 +26,10 @@ class calendarFragment : Fragment() {
         todoAdapter=TodoAdapter(todoList)
         binding?.recTodo?.layoutManager=LinearLayoutManager(context)
         binding?.recTodo?.adapter=todoAdapter
-        getTDContentData()
+
     }
-    private fun getTDContentData() {
-        val database = Firebase.database
-        val todoRef=database.getReference("todo")
-        todoRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val value=dataSnapshot.getValue<String>()
-                Log.d("calenderFragment", "item: ${value}")
-                    // 리스트에 읽어 온 데이터를 넣어준다.
-                // notifyDataSetChanged()를 호출하여 adapter에게 값이 변경 되었음을 알려준다.
-                todoAdapter.replaceList(todoList)
-            }
-            override fun onCancelled(error: DatabaseError) {
-            }
-        })
+
         // addValueEventListener() 메서드로 DatabaseReference에 ValueEventListener를 추가한다.
-    }
     override fun onDestroyView() {
         super.onDestroyView()
         binding=null
--- app/src/main/java/com/example/scheduleapp/todoFragment.kt
+++ app/src/main/java/com/example/scheduleapp/todoFragment.kt
@@ -1,16 +1,11 @@
 package com.example.scheduleapp
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.example.scheduleapp.databinding.FragmentTodoBinding
-import com.google.firebase.Firebase
-import com.google.firebase.database.database
 
 class todoFragment : Fragment() {
     private lateinit var binding:FragmentTodoBinding
@@ -23,20 +18,5 @@ class todoFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        inView()
-    }
-    private fun inView(){
-        binding.finishTodo.setOnClickListener{
-            val database = Firebase.database
-            val todoRef=database.getReference("todo")
-            val todoName = binding.tName.text.toString()
-            val todoDate = binding.tDate.text.toString()
-
-            todoRef.push().setValue(TodoList(todoName,todoDate))
-            Log.d(TAG,"todoRef::$todoRef")
-            binding.tName.text.clear()
-            binding.tDate.text.clear()
-            findNavController().navigate(R.id.action_todoFragment_to_calendarFragment)
-        }
     }
 }
